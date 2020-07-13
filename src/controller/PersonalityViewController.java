b3844930974ce4ba06ac75c4136a696010695204
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-13 19:32:45 +0530

    Add files via upload

--- src/controller/PersonalityViewController.java
+++ src/controller/PersonalityViewController.java
@@ -1,6 +1,8 @@
 package controller;
 
+
 import java.io.IOException;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,7 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.PersonalityCalculator;
 
+import service.PersonalityCalculator;
 @WebServlet(urlPatterns= {"/personality"})
 public class PersonalityViewController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -33,27 +37,28 @@ public class PersonalityViewController extends HttpServlet {
 		String options=request.getParameter("selectedOptions");
 		
 		System.out.println(options);
-				
-		/* 
-
+		PersonalityCalculator pc = new PersonalityCalculator();
+		String message = pc.findYourBrainType(options);
 		request.setAttribute("message", message);
+				
 		
-		if(message!=null)
-		{
+
+		
+	if(message!=null)
+	{
 			if(message.equals("rightbrained")) {
-				RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/rightBrainView.jsp");
-				rd.forward(request, response);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/rightBrainView.jsp");
+			rd.forward(request, response);
 			}
 			else if(message.equals("leftbrained")) {
 				RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/leftBrainView.jsp");
-				rd.forward(request, response);
+			rd.forward(request, response);
 			}
 			else {
-				RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/neutralView.jsp");
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/neutralView.jsp");
 				rd.forward(request, response);
-			}
-		
-		}*/
+		}
+	}
 		}
 	}
 
